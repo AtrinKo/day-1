376ead6a49e2ee6a5b94ca753076db9cf31d1171
Amir Reza kord <akord927@gmail.com>
2024-02-25 22:00:46 +0330

    changed the css and js

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
-const container = document.querySelector(".container");
 const images = document.querySelectorAll(".img");
 
-document.addEventListener("click", classHandler);
+// document.addEventListener("click", classHandler);
 
-function classHandler(event) {
-  for (let key in images) {
-    if (event.target == images[key]) {
-      event.target.classList.add("active");
-      event.target.childNodes[0].classList.add("header-active");
-      for (let key in images) {
-        if (event.target != images[key]) {
-          images[key].classList.remove("active");
-          images[key].childNodes[0].classList.remove("header-active");
-        }
-      }
-    }
-  }
+// function classHandler(event) {
+//   for (let key in images) {
+//     if (event.target == images[key]) {
+//       event.target.classList.add("active");
+//       event.target.childNodes[0].classList.add("header-active");
+//       for (let key in images) {
+//         if (event.target != images[key]) {
+//           images[key].classList.remove("active");
+//           images[key].childNodes[0].classList.remove("header-active");
+//         }
+//       }
+//     }
+//   }
+// }
+
+images.forEach((item) => {
+  item.addEventListener("click", () => {
+    removeClasses();
+    item.classList.add("active");
+    item.childNodes[0].classList.add("header-active");
+  });
+});
+
+function removeClasses() {
+  images.forEach((item) => {
+    item.classList.remove("active");
+    item.childNodes[0].classList.remove("header-active");
+  });
 }
